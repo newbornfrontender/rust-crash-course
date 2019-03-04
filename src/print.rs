ca7c8dd50bc13d04b2547b97ebb7d9632807526f
Infant Frontender <newbornfrontender@gmail.com>
2019-03-04 19:20:48 +0300

    style: format code

--- src/print.rs
+++ src/print.rs
@@ -9,7 +9,11 @@ pub fn run() {
     );
 
     // Named arguments
-    println!("{name} likes to play {activity}", name ="John", activity = "Baseball");
+    println!(
+        "{name} likes to play {activity}",
+        name = "John",
+        activity = "Baseball"
+    );
 
     // Placeholder traits
     println!("Binary: {:b} Hex: {:x} Octal: {:o}", 10, 10, 10);
