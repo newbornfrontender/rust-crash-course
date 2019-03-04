f2d7b83910eced76407f10cf2b8a44e9080d4703
Infant Frontender <newbornfrontender@gmail.com>
2019-03-04 19:00:03 +0300

    chore: add named arguments

--- src/print.rs
+++ src/print.rs
@@ -7,4 +7,7 @@ pub fn run() {
         "{0} is from {1} and {0} likes to {2}",
         "Brad", "Mass", "code"
     );
+
+    // Named arguments
+    println!("{name} likes to play {activity}", name ="John", activity = "Baseball")
 }
