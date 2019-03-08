0c118ac4a60c957ed9bb57c6ec310a7fa8ce800e
Infant Frontender <newbornfrontender@gmail.com>
2019-03-08 16:04:16 +0300

    feat: add const

--- src/vars.rs
+++ src/vars.rs
@@ -2,10 +2,14 @@ pub fn run() {
   let name = "Tom";
   let mut age = 37;
 
-
   println!("My name is {} and I am {}", name, age);
 
   age = 38;
 
   println!("My name is {} and I am {}", name, age);
+
+  // Define constant
+  const ID: i32 = 001;
+
+  println!("ID: {}", ID);
 }
