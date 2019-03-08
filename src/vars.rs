741c54834f10d1e29ad2b643974d56661924d8a1
Infant Frontender <newbornfrontender@gmail.com>
2019-03-08 16:06:51 +0300

    feat: add multiple vars

--- src/vars.rs
+++ src/vars.rs
@@ -12,4 +12,9 @@ pub fn run() {
   const ID: i32 = 001;
 
   println!("ID: {}", ID);
+
+  // Assign multiple vars
+  let ( my_name, my_age ) = ("Brad", 38);
+
+  println!("My name is {} and I am {}", my_name, my_age);
 }
